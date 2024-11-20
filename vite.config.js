d17a4ab44222c183f682d95ca903a4e8e3a35266
jasmeetsingh23 <js2365536@gmail.com>
2024-11-20 13:41:05 +0530

    333333334444

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Listen on all network interfaces
+    port: 5173, // The port Vite will run on
+  },
+});
